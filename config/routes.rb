b0287314d7cf430d3a86fce773f090df527b79ac
Yu <cult.1209.right.u@gmail.com>
2025-07-29 15:43:33 +0900

    Day16（デバッグツール導入、haml、annotate、rubocop、better_errorsの導入）

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  root to: 'articles#index'
+  root to: "articles#index"
   resources :articles
   # get '/about' => 'articles#about'
 end
